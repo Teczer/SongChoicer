c274001d3cde72d13c279ab92998e23eea9ada46
Teczer <mehdi.hattou1@gmail.com>
2024-06-01 20:54:54 +0200

    :green_heart: Layout : Making versus Layout Prototype

--- components/SongButton.tsx
+++ components/SongButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
 import { Song } from "@/app/lib/types";
+import Image from "next/image";
 
 interface SongButtonProps {
   song: Song;
@@ -9,9 +9,21 @@ interface SongButtonProps {
 
 const SongButton: React.FC<SongButtonProps> = ({ song, onVote }) => {
   return (
-    <Button variant={"outline"} onClick={() => onVote(song.id)}>
-      {song.title}
-    </Button>
+    <div
+      className="w-1/4 flex flex-col items-center justify-center border rounded-lg p-10 gap-5 transition-all cursor-pointer hover:scale-105 dark:border-white/[0.1]"
+      onClick={() => onVote(song.id)}
+    >
+      <Image
+        className="rounded-sm"
+        src={
+          "https://images.genius.com/9bbc999c233ae1793aab2167c0202ae7.1000x1000x1.png"
+        }
+        alt={song.title}
+        width={400}
+        height={400}
+      />
+      <p className="font-bold text-lg">{song.title}</p>
+    </div>
   );
 };
 
