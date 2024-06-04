b7d1aa3d084391b83bc3c6803eede140b5e4e067
Teczer <mehdi.hattou1@gmail.com>
2024-06-04 20:03:54 +0200

    :sparkles: Feature : Desktop users can now Download result image card

--- components/SongRanker.tsx
+++ components/SongRanker.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import html2canvas from "html2canvas";
 import * as htmlToImage from "html-to-image";
 
 import Link from "next/link";
