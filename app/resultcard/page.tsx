2b8191fa6adb06aacef1ab271e20e45ff6e3c618
Teczer <mehdi.hattou1@gmail.com>
2024-06-09 23:05:10 +0200

    :bug: BUGFIX: Wrap resultcard page into Suspense

--- app/resultcard/page.tsx
+++ app/resultcard/page.tsx
@@ -1,10 +1,21 @@
+import { Suspense } from 'react'
+
 import SongResultCard from '@/components/SongResultCard'
 import { AuroraBackground } from '@/components/ui/aurora-background'
+import { AlbumCardSkeleton } from '@/components/AlbumCardSkeleton'
 
 export default function Home() {
   return (
     <AuroraBackground className="pt-16">
-      <SongResultCard />
+      <Suspense
+        fallback={
+          <div className="w-screen min-h-screen flex items-center justify-center">
+            <AlbumCardSkeleton />
+          </div>
+        }
+      >
+        <SongResultCard />
+      </Suspense>
     </AuroraBackground>
   )
 }
