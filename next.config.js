f9af94e075847671524be6b2a2057d5ef641336a
AbderrahmaneMouzoune <abderrahmane.mouzoune@gmail.com>
2025-06-25 21:33:59 +0200

    fix: :adhesive_bandage: fix lint

--- next.config.js
+++ next.config.js
@@ -19,23 +19,23 @@ const withPWA = require('next-pwa')({
 
 const nextConfig = withPWA({
   images: {
-    remotePatterns: hosts.map((host) => ({
+    remotePatterns: hosts.map(host => ({
       hostname: host,
     })),
   },
   async rewrites() {
     return [
       {
-        source: '/ingest/static/:path*',
         destination: 'https://eu-assets.i.posthog.com/static/:path*',
+        source: '/ingest/static/:path*',
       },
       {
-        source: '/ingest/:path*',
         destination: 'https://eu.i.posthog.com/:path*',
+        source: '/ingest/:path*',
       },
       {
-        source: '/ingest/decide',
         destination: 'https://eu.i.posthog.com/decide',
+        source: '/ingest/decide',
       },
     ];
   },
